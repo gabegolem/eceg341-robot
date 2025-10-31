4e8bd20fccb5b69ead9def0e9a5ec1b2dd98ca94
Gabriel Rottet <gtr005@bucknell.edu>
2025-10-31 19:00:01 -0400

    Fixed boot theme

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import driving
 import ultrasound
 import testing
-import threading
 import songs
 
 print("Running")
 
 songs = songs.Songs()
-songs.sing(songs.get_boot_theme(), beat = .6, volume = 1000)
-
 ultrasound = ultrasound.Ultrasound(trigger = machine.Pin(28, machine.Pin.OUT), echo = machine.Pin(7, machine.Pin.IN))
 driving = driving.Driving()
 tests = testing.Tests()
+ultrasound.sing(songs.get_boot_theme(), beat = .6, volume = 1000)
+
 
 tests.test_driving()
